bca2428a8dc2da314ebe9c5db17263041c50dfd0
Beola0 <bea.jel@gmail.com>
2022-03-22 15:22:46 +0100

    [plot] add functions to plot matrices

--- AntineutrinoSpectrum/plot.py
+++ AntineutrinoSpectrum/plot.py
@@ -1,25 +1,32 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as plticker
-import numpy as np
-import sys
 import matplotlib.cm as cm
-from matplotlib.colors import Normalize
+import matplotlib.colors as colors
+import numpy as np
 
-RED = '\033[91m'
-NC = '\033[0m'
+modes = ['log', 'pow_law', 'power_law', 'powerlaw', 'centered', 'norm']
 
 
-def plot_function(x_, y_, ylabel_, label_='', styles=None, xlabel_=r'$E_{\nu}$ [\si{MeV}]',
-                  xlim=None, ylim=None, logx=False, figsize_=[7., 4.], base_major=2.0, base_minor=0.5):
+def _forward(x):
+    scale_ = 0.5
+    return np.sign(x) * np.abs(x)**scale_
+
+
+def _inverse(x):
+    scale_ = 0.5
+    return np.sign(x) * np.abs(x)**(1./scale_)
+
+
+def plot_function(x_, y_, ylabel_, label_='', styles=None, xlabel_=r'$E_{\nu}$ [MeV]',
+                  xlim=None, ylim=None, logx=False, fig_length=7, fig_height=4, base_major=2.0, base_minor=0.5,
+                  constrained_layout_=False):
     if len(x_) != len(y_):
-        print(f"{RED}Error in plot_function: different lengths of x array ({len(x_)}) and y array ({len(y_)}){NC}")
-        print(x_)
-        print(y_)
-        sys.exit()
+        raise ValueError(f"Error in plot_function: different lengths of x array ({len(x_)}) and y array ({len(y_)})")
 
-    fig = plt.figure(figsize=figsize_, constrained_layout=True)
-    # fig.subplots_adjust(left=0.11, right=0.97, top=0.95, bottom=0.14)
-    # fig.subplots_adjust(left=0.11, right=0.91, top=0.95, bottom=0.14)
+    fig = plt.figure(figsize=[fig_length, fig_height], constrained_layout=constrained_layout_)
+    if not constrained_layout_:
+        fig.subplots_adjust(left=0.11, right=0.97, top=0.95, bottom=0.14)
+        # fig.subplots_adjust(left=0.11, right=0.91, top=0.95, bottom=0.14)
     ax_ = fig.add_subplot(111)
     if styles is None:
         styles = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
@@ -50,21 +57,22 @@ def plot_function(x_, y_, ylabel_, label_='', styles=None, xlabel_=r'$E_{\nu}$ [
     return ax_
 
 
-def plot_function_residual(x_, y_, label_, ylabel_, styles=None, xlabel_=r'$E_{\nu}$ [\si{MeV}]',
+def plot_function_residual(x_, y_, label_, ylabel_, styles=None, xlabel_=r'$E_{\nu}$ [MeV]',
                            xlim=None, ylim=None, ylim2=None, logx=False, y2_sci=False,
-                           figsize_=[7, 6], base_major=2.0, base_minor=0.5):
+                           fig_length=7, fig_height=6, base_major=2.0, base_minor=0.5,
+                           constrained_layout_=False):
     if len(x_) != len(y_):
-        print(f"{RED}Error in plot_function: different lengths of x array ({len(x_)}) and y array ({len(y_)}){NC}")
-        sys.exit()
+        raise ValueError(f"Different lengths of x array ({len(x_)}) and y array ({len(y_)})")
 
     if styles is None:
         styles = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
                   '#17becf', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
                   '#bcbd22', '#17becf']
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=figsize_, gridspec_kw={'height_ratios': [2, 1]}, 
-                                   sharex=True, constrained_layout=True)
-    # fig.subplots_adjust(left=0.12, right=0.97, top=0.96, bottom=0.093)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=[fig_length, fig_height], gridspec_kw={'height_ratios': [2, 1]},
+                                   sharex=True, constrained_layout=constrained_layout_)
+    if not constrained_layout_:
+        fig.subplots_adjust(left=0.12, right=0.97, top=0.96, bottom=0.093)
 
     for i_ in np.arange(len(x_)):
         if not logx:
@@ -83,7 +91,6 @@ def plot_function_residual(x_, y_, label_, ylabel_, styles=None, xlabel_=r'$E_{\
             continue
 
     ax1.grid(alpha=0.65)
-    # ax1.set_xlabel(xlabel_)
     ax1.set_ylabel(ylabel_)
     ax2.grid(alpha=0.65)
     ax2.set_xlabel(xlabel_)
@@ -115,10 +122,11 @@ def plot_function_residual(x_, y_, label_, ylabel_, styles=None, xlabel_=r'$E_{\
     return ax1, ax2
 
 
-def plot_matrix(m_, xlabel_, ylabel_, label_='', min_=None, max_=None, cmap_='viridis', origin_='lower',
-                figsize_=[6.15, 5.], title_=None):
+def plot_matrix(m_, xlabel_, ylabel_, label_='', min_=None, max_=None, cmap_='seismic', origin_='lower',
+                fig_length=6.15, fig_height=5, title_=None, mode_='centered', sci_mode_=False, shrink=1.,
+                constrained_layout_=False):
 
-    fig = plt.figure(figsize=figsize_, constrained_layout=True)
+    fig = plt.figure(figsize=[fig_length, fig_height], constrained_layout=constrained_layout_)
     ax_ = fig.add_subplot(111)
 
     if min_ is None:
@@ -127,18 +135,134 @@ def plot_matrix(m_, xlabel_, ylabel_, label_='', min_=None, max_=None, cmap_='vi
     if max_ is None:
         max_ = m_.max()
 
+    limit = max(np.abs(min_), max_)
     cmap = cm.get_cmap(cmap_)
-    normalizer = Normalize(min_, max_)
-    im = cm.ScalarMappable(norm=normalizer, cmap=cmap)
 
-    ax_.imshow(m_, origin=origin_, norm=normalizer, cmap=cmap)
+    if mode_ == 'log':
+        norm = colors.LogNorm(vmin=min_, vmax=max_)
+    elif mode_ == 'pow_law' or mode_ == 'power_law' or mode_ == 'powerlaw':
+        norm = colors.FuncNorm((_forward, _inverse), vmin=-limit, vmax=limit)
+    elif mode_ == 'centered':
+        norm = colors.CenteredNorm()
+    elif mode_ == 'norm':
+        norm = colors.Normalize(vmin=min_, vmax=max_)
+    else:
+        raise ValueError("Invalid mode_ value. Expected one of: %s" % modes)
+
+    im = cm.ScalarMappable(norm=norm, cmap=cmap)
+    ax_.imshow(m_, origin=origin_, norm=norm, cmap=cmap)
+
     ax_.set_xlabel(xlabel_)
     ax_.set_ylabel(ylabel_)
     if title_ is not None:
         ax_.set_title(title_)
-    # ax_.colorbar()
 
-    cbar = fig.colorbar(im, ax=ax_, label=label_)
-    cbar.formatter.set_powerlimits((0, 0))
+    cbar = fig.colorbar(im, ax=ax_, label=label_, shrink=shrink)
+    if sci_mode_:
+        cbar.formatter.set_powerlimits((0, 0))
+    cbar.ax.yaxis.set_offset_position('left')
 
     return ax_
+
+
+def plot_two_matrices(m_, xlabel_, ylabel_, label_='', min_=None, max_=None, cmap_='seismic', origin_='lower',
+                      fig_length=9, fig_height=6, titles_=None, mode_='centered', sci_mode_=False, shrink=0.7,
+                      constrained_layout_=False):
+    if len(m_) != 2:
+        raise ValueError("Invalid input length. Expected input with two matrices.")
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=[fig_length, fig_height], sharex=True, sharey=True,
+                             constrained_layout=constrained_layout_)
+
+    if min_ is None:
+        min_ = min(m_[0].min(), m_[1].min())
+
+    if max_ is None:
+        max_ = min(m_[0].max(), m_[1].max())
+
+    limit = max(np.abs(min_), max_)
+    cmap = cm.get_cmap(cmap_)
+
+    if mode_ == 'log':
+        norm = colors.LogNorm(vmin=min_, vmax=max_)
+    elif mode_ == 'pow_law' or mode_ == 'power_law' or mode_ == 'powerlaw':
+        norm = colors.FuncNorm((_forward, _inverse), vmin=-limit, vmax=limit)
+    elif mode_ == 'centered':
+        norm = colors.CenteredNorm()
+    elif mode_ == 'norm':
+        norm = colors.Normalize(vmin=min_, vmax=max_)
+    else:
+        raise ValueError("Invalid mode_ value. Expected one of: %s" % modes)
+
+    im = cm.ScalarMappable(norm=norm, cmap=cmap)
+    axes[0].imshow(m_[0], origin=origin_, norm=norm, cmap=cmap)
+    axes[1].imshow(m_[1], origin=origin_, norm=norm, cmap=cmap)
+
+    axes[0].set_xlabel(xlabel_)
+    axes[1].set_xlabel(xlabel_)
+    axes[0].set_ylabel(ylabel_)
+    if titles_ is not None:
+        axes[0].set_title(titles_[0])
+        axes[1].set_title(titles_[1])
+
+    cbar = fig.colorbar(im, ax=axes.ravel().tolist(), label=label_, shrink=shrink)
+    if sci_mode_:
+        cbar.formatter.set_powerlimits((0, 0))
+    cbar.ax.yaxis.set_offset_position('left')
+
+    return axes
+
+
+def plot_six_matrices(m_, xlabel_, ylabel_, label_='', min_=None, max_=None, cmap_='seismic', origin_='lower',
+                      fig_length=9, fig_height=6, titles_=None, mode_='centered', sci_mode_=False, shrink=0.7,
+                      constrained_layout_=False):
+    if len(m_) > 6:
+        raise ValueError("Invalid input length. Expected input with no more than 6 matrices.")
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=[fig_length, fig_height], sharex=True, sharey=True,
+                             constrained_layout=constrained_layout_)
+
+    if min_ is None:
+        min_ = m_[-1].min()
+
+    if max_ is None:
+        max_ = m_[-1].max()
+
+    limit = max(np.abs(min_), max_)
+    cmap = cm.get_cmap(cmap_)
+
+    if mode_ == 'log':
+        norm = colors.LogNorm(vmin=min_, vmax=max_)
+    elif mode_ == 'pow_law' or mode_ == 'power_law' or mode_ == 'powerlaw':
+        norm = colors.FuncNorm((_forward, _inverse), vmin=-limit, vmax=limit)
+    elif mode_ == 'centered':
+        norm = colors.CenteredNorm()
+    elif mode_ == 'norm':
+        norm = colors.Normalize(vmin=min_, vmax=max_)
+    else:
+        raise ValueError("Invalid mode_ value. Expected one of: %s" % modes)
+
+    im = cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    for i_, ax in enumerate(axes.flat):
+        try:
+            ax.imshow(m_[i_], origin=origin_, cmap=cmap, norm=norm)
+        except IndexError:
+            continue
+        ax.set_title(titles_[i_])
+        if i_ == 3 or i_ == 4 or i_ == 5:
+            ax.set_xlabel(xlabel_)
+        if i_ == 0 or i_ == 3:
+            ax.set_ylabel(ylabel_)
+
+    cbar = fig.colorbar(im, ax=axes.ravel().tolist(), label=label_, shrink=shrink)
+    if sci_mode_:
+        cbar.formatter.set_powerlimits((0, 0))
+    cbar.ax.yaxis.set_offset_position('left')
+
+    if len(m_) < 6:
+        axes[-1, -1].axis('off')
+    if len(m_) < 5:
+        axes[-1, -2].axis('off')
+
+    return axes
